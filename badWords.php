daaaedf4e4e5595618e8763556eb70b294afa066
IIODERII <oderrisi@gmail.com>
2023-11-27 15:36:20 +0100

    added possibility to introduce text and badword

---
+++ badWords.php
@@ -0,0 +1,36 @@
+<?php
+$testo = $_GET["text"];
+$parola = $_GET["badword"];
+
+$fixedText = str_replace($parola, "***", strtolower($testo));
+$textLength = strlen($fixedText);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Badwords</title>
+</head>
+
+<body>
+    <div class="container p-4">
+        <h1>Ecco il tuo testo senza la parola:
+            <?php echo $parola ?>
+        </h1>
+        <p class="fs-3">
+            <?php echo $fixedText ?>
+        </p>
+
+        <p>Lunghezza testo:
+            <?php echo $textLength ?>
+        </p>
+    </div>
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -3,12 +3,25 @@
 
 <head>
     <meta charset="UTF-8">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>PHP Badwords</title>
 </head>
 
 <body>
-    ciao
+    <form action="badwords.php" method="GET" class="p-4">
+        <label for="text">La tua frase:</label>
+        <br>
+        <textarea name="text" cols="50" rows="10"></textarea>
+        <br>
+        <label for="badword">La parola da rimuovere:</label>
+        <br>
+        <input type="text" name="badword">
+        <br>
+        <input class="btn btn-success my-2 rounded-pill" type="submit">
+    </form>
 </body>
 
 </html>
